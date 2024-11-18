5e92ab7494549c5440540658b97c0c60c7fdaf4c
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-18 23:54:22 +0200

    outputting conditional content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   title = 'EasyTask';
 
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
