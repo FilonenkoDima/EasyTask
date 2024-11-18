ab2a76b902a64ca1739957e777b3dd1bce0d5e7d
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-18 22:56:44 +0200

    added tasks component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -19,7 +19,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   // id = input.required<string>();
   // avatar = input.required<string>();
