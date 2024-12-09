e1600261601f41fb4e177fb6ae859fae6c7ab4e8
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-10 00:46:21 +0200

    changed task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UsersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'EasyTask';
-
-  users = DUMMY_USERS;
-  selectedUserId?: string;
-
-  get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedUserId);
-  }
-
-  onSelectUser(id: string) {
-    this.selectedUserId = id;
-  }
-}
+export class AppComponent {}
