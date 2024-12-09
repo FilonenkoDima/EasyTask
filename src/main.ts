9b4bd712c8b102f0cb2e22c216d49bd32408e488
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-09 23:46:40 +0200

    back to standalone components

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
-// bootstrapApplication(AppComponent).catch((err) => console.error(err));
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
