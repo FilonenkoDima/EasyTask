154def4886f682314de49a30cfa14d53484928d0
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-10 00:04:02 +0200

    rewrite task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,32 +1,15 @@
 import { Component, Input, Output } from '@angular/core';
 
-// import { TaskComponent } from './task/task.component';
-// import { NewTaskComponent } from './new-task/new-task.component';
-import { TasksService } from './tasks.service';
+import { TaskComponent } from './task/task.component';
+import { Task } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
-  // standalone: true,
-  // imports: [TaskComponent, NewTaskComponent],
+  standalone: true,
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: true }) userId!: string;
-  @Input({ required: true }) name?: string;
-  isAddingTask: boolean = false;
-
-  constructor(private tasksService: TasksService) {}
-
-  get selectedUserTasks() {
-    return this.tasksService.getUserTasks(this.userId);
-  }
-
-  onStartAddTask() {
-    this.isAddingTask = true;
-  }
-
-  onCloseAddTask() {
-    this.isAddingTask = false;
-  }
+  userTasks: Task[] = [];
 }
