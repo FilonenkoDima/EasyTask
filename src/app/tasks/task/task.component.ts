5e9012990162050fe5d8e7b9b2103bfda99730f7
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-10 00:17:47 +0200

    migrated task component to signal

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,22 +1,22 @@
-import { Component, inject, Input } from '@angular/core';
-// import { DatePipe } from '@angular/common';
+import { Component, inject, input, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 import { type Task } from './task.model';
-// import { CardComponent } from '../../shared/card/card.component';
+import { CardComponent } from '../../shared/card/card.component';
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  // standalone: true,
-  // imports: [CardComponent, DatePipe],
+  standalone: true,
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input({ required: true }) task!: Task;
+  task = input.required<Task>();
   private tasksService = inject(TasksService);
 
   onCompleteTask() {
-    this.tasksService.removeTask(this.task.id);
+    this.tasksService.removeTask(this.task().id);
   }
 }
