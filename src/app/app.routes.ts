26f9e521712e56ad0ea6a9b2840362543c22b3c4
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-12-13 16:02:09 +0200

    route related dynamic data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,10 @@ import { Routes } from '@angular/router';
 
 import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import {
+  resolveUserName,
+  UserTasksComponent,
+} from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
@@ -14,6 +17,12 @@ export const routes: Routes = [
     path: 'users/:userId', // <domain>/users/<uid>
     component: UserTasksComponent,
     children: userRoutes,
+    data: {
+      message: 'Hello!',
+    },
+    resolve: {
+      userName: resolveUserName,
+    },
   },
   {
     path: '**',
